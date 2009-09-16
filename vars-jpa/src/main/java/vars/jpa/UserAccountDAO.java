c63358611eb35ef74b759542ce6883e929e017a1
Brian Schlining <brian@mbari.org>
2009-09-17 05:38:18 +0700

    Integrating changes for SIMPA testing

--- vars-jpa/src/main/java/vars/jpa/UserAccountDAO.java
+++ vars-jpa/src/main/java/vars/jpa/UserAccountDAO.java
@@ -1,13 +1,26 @@
+/*
+ * @(#)UserAccountDAO.java   2009.09.02 at 04:26:52 PDT
+ *
+ * Copyright 2009 MBARI
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
 package vars.jpa;
 
-import org.mbari.jpax.EAO;
-import vars.IUserAccountDAO;
-import vars.IUserAccount;
-
 import java.util.Collection;
-import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.mbari.jpax.EAO;
+import vars.IUserAccount;
+import vars.IUserAccountDAO;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,8 +31,37 @@ import java.util.List;
  */
 public class UserAccountDAO extends DAO implements IUserAccountDAO {
 
+    /**
+     * Constructs ...
+     *
+     * @param eao
+     */
     public UserAccountDAO(EAO eao) {
         super(eao);
+    }
+
+    public Collection<IUserAccount> findAllByFirstName(String firstName) {
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        params.put("firstName", firstName);
+
+        return getEAO().findByNamedQuery("UserAccount.findByFirstName", params);
+    }
+
+    public Collection<IUserAccount> findAllByLastName(String lastName) {
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        params.put("lastName", lastName);
+
+        return getEAO().findByNamedQuery("UserAccount.findByLastName", params);
+    }
+
+    public Collection<IUserAccount> findAllByRole(String role) {
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        params.put("role", role);
+
+        return getEAO().findByNamedQuery("UserAccount.findByRole", params);
     }
 
     /**
@@ -29,27 +71,11 @@ public class UserAccountDAO extends DAO implements IUserAccountDAO {
      */
     public IUserAccount findByUserName(String userName) {
         Map<String, Object> params = new HashMap<String, Object>();
+
         params.put("userName", userName);
+
         List<IUserAccount> accounts = getEAO().findByNamedQuery("UserAccount.findByUserName", params);
+
         return (accounts.size() == 0) ? null : accounts.get(0);
     }
-
-    public Collection<IUserAccount> findAllByLastName(String lastName) {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("lastName", lastName);
-        return getEAO().findByNamedQuery("UserAccount.findByLastName", params);
-    }
-
-    public Collection<IUserAccount> findAllByFirstName(String firstName) {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("firstName", firstName);
-        return getEAO().findByNamedQuery("UserAccount.findByFirstName", params);
-    }
-
-    public Collection<IUserAccount> findAllByRole(String role) {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("role", role);
-        return getEAO().findByNamedQuery("UserAccount.findByRole", params);
-    }
-
 }
