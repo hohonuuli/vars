3ea6f6e18878d2e1d39460fa5d069fe9062b0e87
Brian Schlining <brian@mbari.org>
2011-06-27 23:57:49 +0700

    Added BUG FIX for bad uniqueid keys

---
+++ vars-standalone/src/main/assembly/files/scripts/groovy/increment_uniqueid.groovy
@@ -0,0 +1,15 @@
+import groovy.sql.Sql
+
+def bundle = ResourceBundle.getBundle("annotation-jdbc")
+def database = Sql.newInstance(bundle.getString("jdbc.url"),
+        bundle.getString("jdbc.username"),
+        bundle.getString("jdbc.password"),
+        bundle.getString("jdbc.driver"))
+        
+def rows = database.rows('SELECT tablename, nextid FROM UniqueID')
+rows.each { row ->
+    database.execute """
+        UPDATE UniqueID SET nextid = ${row.nextid + 100} WHERE tablename = ${row.tablename}
+    """
+}
+
