53ee88a3f95d884375e3a69009759320a2b6392a
Brian Schlining <brian@mbari.org>
2011-11-02 03:57:58 +0700

    Added a script to easily change passwords.

---
+++ vars-standalone/src/main/assembly/files/scripts/groovy/change_password.groovy
@@ -0,0 +1,31 @@
+import org.jasypt.util.password.BasicPasswordEncryptor
+import org.mbari.sql.QueryFunction
+import vars.ToolBox;
+
+if (args.size() != 2) {
+    println("""
+    Usage: gsh change_password <username> <newPassword>
+    
+    Args:
+        username = The user whos password needs to be changed
+        newPassword = The new password
+    """)
+    return
+}
+
+def name = args[0]
+def password = args[1]
+
+def toolBelt = new vars.ToolBox().toolBelt
+def dao = toolBelt.miscDAOFactory.newUserAccountDAO()
+dao.startTransaction()
+def userAccount = dao.findByUserName(name)
+if (userAccount) {
+    userAccount.password = password
+}
+else {
+    println("Unable to find user named: ${name}")
+}
+dao.endTransaction()
+
+
