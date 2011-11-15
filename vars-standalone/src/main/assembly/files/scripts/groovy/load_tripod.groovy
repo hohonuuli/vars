cb41da80416625c461542456fd380c8bf7c1e8df
Brian Schlining <brian@mbari.org>
2011-11-16 06:57:26 +0800

    The loader for tripod images has been completed

--- vars-standalone/src/main/assembly/files/scripts/groovy/load_tripod.groovy
+++ vars-standalone/src/main/assembly/files/scripts/groovy/load_tripod.groovy
@@ -9,7 +9,8 @@ import org.mbari.vars.tripod.TripodLoader
 if (args.size() < 6) {
     println("""
     | Usage:
-    |   gsh load_tripod <url> <sequence number> <longitude> <latitude> <lens to seafloor distance> <angle of inclination of camera>
+    |   gsh load_tripod <url> <sequence number> <longitude> <latitude> <depth> \
+    |       <lens to seafloor distance> <angle of inclination of camera>
     |
     | Args:
     |   url = The url that gives a directory listing of the images to load
@@ -17,6 +18,7 @@ if (args.size() < 6) {
     |   sequence number = the Deployment number (e.g. 55)
     |   longitude = The longitude of the deployed camera
     |   latitude = The latitude of the deployed camera
+    |   depth = The tripods depth in meters
     |   lens to seafloor distance = Distance in meters from the lens to the seafloor.
     |   angle of inclination = Angle of the camera in degrees, from level horizontal, towards seafloor.
     |                          downwards is positive.
@@ -30,11 +32,12 @@ def remoteDirectoryUrl = new URL(args[0])
 def platform = 'Tripod M'
 def sequenceNumber = Integer.parseInt(args[1])
 def longitude = Double.parseDouble(args[2])
-def latitude = Double.parseDouble([3])
-def lensToSeaFloorDistance = Double.parseDouble(args[4])
-def angleOfInclination = Double.parseDouble(args[5])
+def latitude = Double.parseDouble(args[3])
+def depth = Double.parseDouble(args[4])
+def lensToSeaFloorDistance = Double.parseDouble(args[5])
+def angleOfInclination = Double.parseDouble(args[6])
 
 def loader = new TripodLoader()
-loader.load(remoteDirectoryUrl, platform, sequenceNumber, longitude, latitude, lensToSeaFloorDistance, angleOfInclination)
+loader.load(remoteDirectoryUrl, platform, sequenceNumber, longitude, latitude, depth, lensToSeaFloorDistance, angleOfInclination)
 
 
--- vars-standalone/src/main/assembly/files/scripts/groovy/org/mbari/vars/tripod/TripodLoader.groovy
+++ vars-standalone/src/main/assembly/files/scripts/groovy/org/mbari/vars/tripod/TripodLoader.groovy
@@ -12,6 +12,7 @@ import vars.annotation.PhysicalData
 import vars.annotation.VideoArchive
 import vars.annotation.Observation
 import vars.knowledgebase.ConceptName
+import org.mbari.movie.Timecode
 
 /**
  *
@@ -20,94 +21,104 @@ import vars.knowledgebase.ConceptName
  */
 class TripodLoader {
 
-def toolBox = new ToolBox()
-def dateFormat = new SimpleDateFormat('yyyy:MM:dd HH:mm:ss')
-def timecodeFormat = new SimpleDateFormat("HH:mm:ss:'00'")
+    def toolBox = new ToolBox()
+    def dateFormat = new SimpleDateFormat('yyyy:MM:dd HH:mm:ss')
+    def timecodeFormat = new SimpleDateFormat("HH:mm:ss:'00'")
 
-def TripodLoader() {
-    dateFormat.timeZone = TimeZone.getTimeZone('UTC')
-    timecodeFormat.timeZone = TimeZone.getTimeZone('UTC')
-}
+    def TripodLoader() {
+        dateFormat.timeZone = TimeZone.getTimeZone('UTC')
+        timecodeFormat.timeZone = TimeZone.getTimeZone('UTC')
+    }
 
-def load(URL remoteImageDirectory,
-         String platform,
-         Integer sequenceNumber,
-         Double longitude,
-         Double latitude,
-         Double lensToSeafloorDistance,
-         Double angleOfInclination) {
-    def annotationFactory = toolBox.toolBelt.annotationFactory
-    def videoArchiveName = PersistenceController.makeVideoArchiveName(platform, sequenceNumber, 1, "-tripod")
-    try {
-        def conceptDAO = toolBox.toolBelt.knowledgebaseDAOFactory.newConceptDAO()
-        conceptDAO.startTransaction()
-        def conceptNameAsString = conceptDAO.findRoot().primaryConceptName.name
-        conceptDAO.close()
-        def videoArchiveDAO = toolBox.toolBelt.annotationDAOFactory.newVideoArchiveDAO()
-        videoArchiveDAO.startTransaction()
+    def load(URL remoteImageDirectory,
+             String platform,
+             Integer sequenceNumber,
+             Double longitude,
+             Double latitude,
+             Double depth,
+             Double lensToSeafloorDistance,
+             Double angleOfInclination) {
+        def annotationFactory = toolBox.toolBelt.annotationFactory
+        def videoArchiveName = PersistenceController.makeVideoArchiveName(platform, sequenceNumber, 1, "-tripod")
+        try {
+            def conceptDAO = toolBox.toolBelt.knowledgebaseDAOFactory.newConceptDAO()
+            conceptDAO.startTransaction()
+            def conceptNameAsString = conceptDAO.findRoot().primaryConceptName.name
+            conceptDAO.close()
+            def videoArchiveDAO = toolBox.toolBelt.annotationDAOFactory.newVideoArchiveDAO()
+            videoArchiveDAO.startTransaction()
 
-        VideoArchive videoArchive = videoArchiveDAO.findOrCreateByParameters(platform, sequenceNumber, videoArchiveName)
+            VideoArchive videoArchive = videoArchiveDAO.findOrCreateByParameters(platform, sequenceNumber, videoArchiveName)
 
-        // Parse URLs of images from directory listing
-        def images = fetchImagesURLs(remoteImageDirectory)
-        def observationDate = new Date()
-        //
-        images.each { imageRef ->
-            def url = new URL(imageRef)
-            def metadata = Sanselan.getMetadata(url.openStream(), "FOO")
-            def creationDate = metadata.findEXIFValue(TiffConstants.EXIF_TAG_CREATE_DATE)
-            def date = dateFormat.parse(creationDate)
-            def timecode = timecodeFormat.parse(creationDate)
-            VideoFrame videoFrame = annotationFactory.newVideoFrame()
-            videoFrame.recordedDate = date
-            videoFrame.timecode = timecode
-            CameraData cameraData = videoFrame.cameraData
-            cameraData.z = lensToSeafloorDistance
-            cameraData.pitch = angleOfInclination
-            PhysicalData physicalData = videoFrame.physicalData
-            physicalData.latitude = latitude
-            physicalData.longitude = longitude
-            videoArchive.addVideoFrame(videoFrame)
-            videoArchiveDAO.persist(videoFrame)
-            Observation observation = annotationFactory.newObservation()
-            observation.conceptName = conceptNameAsString
-            observation.observationDate = observationDate
-            observation.observer = getClass().simpleName
-            videoFrame.addObservation(observation)
-            videoArchiveDAO.persist(observation)
+            // Parse URLs of images from directory listing
+            def images = fetchImagesURLs(remoteImageDirectory)
+            def observationDate = new Date()
+            //
+            images.eachWithIndex { imageRef, idx ->
+                print("Processing ${imageRef}")
+                def url = new URL(imageRef)
+                def metadata = Sanselan.getMetadata(url.openStream(), "FOO")
+                def creationDate = metadata.findEXIFValue(TiffConstants.EXIF_TAG_CREATE_DATE)
+                def date = dateFormat.parse(creationDate.valueDescription[1..-2])
+                def timecode = new Timecode(idx, 30)
+                VideoFrame videoFrame = videoArchive.findVideoFrameByTimeCode(timecode.toString())
+                if (videoFrame == null) {
+                    videoFrame = annotationFactory.newVideoFrame()
+                    videoFrame.timecode = timecode.toString()
+                    videoArchive.addVideoFrame(videoFrame)
+                    videoArchiveDAO.persist(videoFrame)
+                }
+                videoFrame.recordedDate = date
+                CameraData cameraData = videoFrame.cameraData
+                cameraData.z = lensToSeafloorDistance
+                cameraData.pitch = angleOfInclination
+                cameraData.imageReference = url.toExternalForm()
+                PhysicalData physicalData = videoFrame.physicalData
+                physicalData.latitude = latitude
+                physicalData.longitude = longitude
+                physicalData.depth = depth
+                print(" ... ${videoFrame.timecode}")
+                Observation observation = annotationFactory.newObservation()
+                observation.conceptName = conceptNameAsString
+                observation.observationDate = observationDate
+                observation.observer = getClass().simpleName
+                videoFrame.addObservation(observation)
+                videoArchiveDAO.persist(observation)
+                println(" ... done")
+            }
+
+            videoArchiveDAO.endTransaction()
+            videoArchiveDAO.close()
         }
-
-        videoArchiveDAO.endTransaction()
-        videoArchiveDAO.close()
-    }
-    catch (Exception e) {
-        println("Failed to process images in ${remoteImageDirectory}")
-    }
-}
-
-def fetchImagesURLs(URL remoteDirectory) {
-    def remotePath = remoteDirectory.toExternalForm()
-    if (!remotePath.endsWith('/')) {
-        remotePath = remotePath + "/"
-    }
-    def images = []
-    def slurper = new XmlSlurper(new Parser())
-    remoteDirectory.withReader { reader ->
-        def html = slurper.parse(reader)
-        def rows = html.body.table.tr
-        rows.each { row ->
-            try {
-                // A is an anchor/link to an image
-                def a = row.td[1].a
-                images << remotePath + a
-            }
-            catch (Exception e) {
-                e.printStackTrace()
-            }
+        catch (Exception e) {
+            println("\nFailed to process images in ${remoteImageDirectory}")
+            e.printStackTrace()
         }
     }
-    return images.findAll { it.toUpperCase().endsWith("JPG")}
-}
+
+    def fetchImagesURLs(URL remoteDirectory) {
+        def remotePath = remoteDirectory.toExternalForm()
+        if (!remotePath.endsWith('/')) {
+            remotePath = remotePath + "/"
+        }
+        def images = []
+        def slurper = new XmlSlurper(new Parser())
+        remoteDirectory.withReader { reader ->
+            def html = slurper.parse(reader)
+            def rows = html.body.table.tr
+            rows.each { row ->
+                try {
+                    // A is an anchor/link to an image
+                    def a = row.td[1].a
+                    images << remotePath + a
+                }
+                catch (Exception e) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        return images.findAll { it.toUpperCase().endsWith("JPG")}
+    }
 
 
 }
