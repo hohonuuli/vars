d1a8963cc8a0eb5bc227e3b3797811afe33e7c8a
Brian Schlining <brian@mbari.org>
2012-09-11 05:28:05 +0700

    JIRA: VARS-548: Added lookup_taxa script

---
+++ vars-standalone/src/main/assembly/files/scripts/groovy/vars/knowledgebase/KBUtilities.groovy
@@ -0,0 +1,33 @@
+package vars.knowledgebase
+
+import vars.ToolBox
+
+/**
+ * 
+ * @author Brian Schlining
+ * @since 2012-09-10
+ */
+class KBUtilities {
+
+    static findConceptsByRank(rank) {
+        def tb = new ToolBox()
+        def dao = tb.toolBelt.knowledgebaseDAOFactory.newConceptDAO()
+        def allConcepts = dao.findAll()
+        return allConcepts.findAll { c ->
+            def r = "${c.rankLevel}${c.rankName}"
+            return r.contains(rank)
+        }
+
+    }
+
+    static showConceptsByRank(rank) {
+        def concepts = findConceptsByRank(rank)
+        println("-" * 72)
+        println("Found ${concepts.size()} concepts with a rank of '${rank}'")
+        concepts.each { c ->
+            def r = (c.rankLevel ?: "") + c.rankName
+            println("${c.primaryConceptName.name}\t[$r]")
+        }
+    }
+
+}
