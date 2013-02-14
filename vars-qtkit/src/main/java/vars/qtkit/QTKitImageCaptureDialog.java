55cec81f3041a57bb831d90c0e983f85baec6892
Brian Schlining <brian@mbari.org>
2013-02-15 01:05:07 +0800

    added pack() to dialog

--- vars-qtkit/src/main/java/vars/qtkit/QTKitImageCaptureDialog.java
+++ vars-qtkit/src/main/java/vars/qtkit/QTKitImageCaptureDialog.java
@@ -43,7 +43,7 @@ public class QTKitImageCaptureDialog extends StandardDialog {
                 setVisible(false);
             }
         });
-
+        pack();
     }
 
     public JPanel getPanel() {
