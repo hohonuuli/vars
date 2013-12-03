4e7d34e455b6719a2129a8faecb82e666cb2b8dc
Brian Schlining <bschlining@gmail.com>
2013-12-03 13:53:17 -0800

    check point

---
+++ vars-avfoundation/src/main/java/vars/avfoundation/DemoApp.java
@@ -0,0 +1,40 @@
+package vars.avfoundation;
+
+import org.mbari.util.TimeUtilities;
+import vars.shared.ui.video.ImageCaptureService;
+
+import java.awt.*;
+import java.io.File;
+import java.text.DateFormat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brian
+ * Date: 12/3/13
+ * Time: 11:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DemoApp {
+
+    public static void main(String[] args) {
+        File saveDirectory = new File(args[0]);
+        if (!saveDirectory.exists()) {
+            saveDirectory.mkdirs();
+        }
+        AVFImageCaptureServiceImpl imageCaptureService = new AVFImageCaptureServiceImpl();
+        String[] videoDeviceNames = imageCaptureService.videoDevicesAsStrings();
+        String videoDevice = videoDeviceNames[0];
+        imageCaptureService.startSessionWithNamedDevice(videoDevice);
+        for (int i = 0; i < 5; i++) {
+            File target = new File(saveDirectory, "snapshot" + i + ".png");
+            Image image = imageCaptureService.capture(target);
+            if (image == null) {
+                System.out.println("Failed to write " + target.getAbsolutePath());
+            }
+            else {
+                System.out.println("Wrote " + target.getAbsolutePath());
+            }
+        }
+
+    }
+}
