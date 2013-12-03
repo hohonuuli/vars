6a45a38b6fde3aa2e7cfe4607e108e63c79921fb
Brian Schlining <bschlining@gmail.com>
2013-12-03 15:52:45 -0800

    check point

---
+++ vars-avfoundation/src/main/xcode/avimagesnap/AVFCocoaWindowController.h
@@ -0,0 +1,13 @@
+//
+//  AVFCocoaWindowController.h
+//  avimagesnap
+//
+//  Created by Brian Schlining on 12/3/13.
+//  Copyright (c) 2013 Brian Schlining. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVFCocoaWindowController : NSObject
+
+@end
---
+++ vars-avfoundation/src/main/xcode/avimagesnap/AVFCocoaWindowController.m
@@ -0,0 +1,13 @@
+//
+//  AVFCocoaWindowController.m
+//  avimagesnap
+//
+//  Created by Brian Schlining on 12/3/13.
+//  Copyright (c) 2013 Brian Schlining. All rights reserved.
+//
+
+#import "AVFCocoaWindowController.h"
+
+@implementation AVFCocoaWindowController
+
+@end
