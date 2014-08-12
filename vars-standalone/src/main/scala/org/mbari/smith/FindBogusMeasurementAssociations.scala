a991167c35d3d87200d7715a3ec95715e8d9bbfe
Brian Schlining <bschlining@gmail.com>
2014-08-12 13:19:32 -0700

    check point

---
+++ vars-standalone/src/main/scala/org/mbari/smith/FindBogusMeasurementAssociations.scala
@@ -0,0 +1,41 @@
+package org.mbari.smith
+
+import com.google.inject.Injector
+import vars.annotation.ui.{ToolBelt, Lookup}
+import vars.annotation.ui.imagepanel.Measurement
+import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
+
+/**
+ * Created by brian on 8/6/14.
+ */
+object FindBogusMeasurementAssociations extends App {
+
+
+  val toolBelt = Lookup.getGuiceInjectorDispatcher.getValueObject.
+      asInstanceOf[Injector].getInstance(classOf[ToolBelt])
+  val dao = toolBelt.getAnnotationDAOFactory.newAssociationDAO()
+  val videoArchiveName = args(0)
+  val videoFrames = CoverageEstimator.fetchAnnotations(videoArchiveName)
+  val associations = for {
+    vf <- videoFrames
+    obs <- vf.getObservations.asScala
+    ass <- obs.getAssociations.asScala
+    if ass.getLinkName == Measurement.MEASUREMENT_LINKNAME
+  } yield ass
+
+  val t = associations.map { a =>
+    Try(Measurement.fromLink(a)) match {
+      case Success(m) => Option(m)
+      case Failure(e) => {
+        println(s"FAIL: ${a.getPrimaryKey}: $a")
+        dao.startTransaction()
+        val b = dao.find(a)
+        b.setLinkValue(a.getLinkValue + " undefined")
+        dao.commit()
+        dao.endTransaction()
+      }
+    }
+
+  }
+}
