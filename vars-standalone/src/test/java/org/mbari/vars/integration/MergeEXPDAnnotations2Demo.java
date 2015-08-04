28a792ea9631be0986dd6b23fcf93c2f077a1ac3
Brian Schlining <bschlining@gmail.com>
2015-08-04 09:21:02 -0700

    check point

---
+++ vars-standalone/src/test/java/org/mbari/vars/integration/MergeEXPDAnnotations2Demo.java
@@ -0,0 +1,16 @@
+package org.mbari.vars.integration;
+
+import vars.integration.MergeType;
+
+/**
+ * @author Brian Schlining
+ * @since 2015-08-04T09:09:00
+ */
+public class MergeEXPDAnnotations2Demo {
+
+
+    public static void main(String[] args) {
+        MergeEXPDAnnotations2 m = new MergeEXPDAnnotations2("Ventana", 148, false);
+        m.apply(MergeType.PESSIMISTIC);
+    }
+}
