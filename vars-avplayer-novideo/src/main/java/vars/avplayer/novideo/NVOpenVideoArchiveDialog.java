a6ec3ef9eab136a0fd060c7bde6abca4b85b49bf
Brian Schlining <bschlining@gmail.com>
2016-01-26 11:51:15 -0800

    Working on Mini ROV support

---
+++ vars-avplayer-novideo/src/main/java/vars/avplayer/novideo/NVOpenVideoArchiveDialog.java
@@ -0,0 +1,8 @@
+package vars.avplayer.novideo;
+
+/**
+ * @author Brian Schlining
+ * @since 2016-01-25T12:00:00
+ */
+public class NVOpenVideoArchiveDialog {
+}
---
+++ vars-avplayer-novideo/src/main/java/vars/avplayer/novideo/NVOpenVideoArchiveDialogController.java
@@ -0,0 +1,8 @@
+package vars.avplayer.novideo;
+
+/**
+ * @author Brian Schlining
+ * @since 2016-01-25T12:07:00
+ */
+public class NVOpenVideoArchiveDialogController {
+}
